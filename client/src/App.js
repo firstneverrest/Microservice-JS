ead09874984da7c8855eaee8319132a8e154f109
Chitsanupong <c.tangvasinkul@gmail.com>
2022-05-20 14:23:39 +0700

    feat: add skaffold

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import PostCreate from "./PostCreate";
-import PostList from "./PostList";
+import React from 'react';
+import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
+      <p>By: Neverrest</p>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
